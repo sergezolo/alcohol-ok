0fdd14ee8a0a07366f95dd07c304694190cf2554
sergezolo <zolotukhinsereg@gmail.com>
2020-09-01 16:26:23 -0500

    Test Coctail #index and #show views

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,2 +1,4 @@
 class Cocktail < ApplicationRecord
+   
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :cocktail_ingredients
   resources :ingredients
-  resources :cocktails
+  resources :cocktails, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
