48e98e3a3344f8b9fb8d165e4c96d6da4d854d95
sergezolo <zolotukhinsereg@gmail.com>
2020-09-01 13:08:52 -0500

    Add CocktailIngredient resource

---
+++ db/migrate/20200901180834_create_cocktail_ingredients.rb
@@ -0,0 +1,12 @@
+class CreateCocktailIngredients < ActiveRecord::Migration[6.0]
+  def change
+    create_table :cocktail_ingredients do |t|
+      t.integer :cocktail_id
+      t.integer :ingredient_id
+      t.integer :quantity
+      t.string :unit
+
+      t.timestamps
+    end
+  end
+end
